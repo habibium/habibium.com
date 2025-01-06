0bfafd7e229ce1c93da508ad643f54bbd0fc901f
habibium <habib.prodev@gmail.com>
2025-01-06 19:31:54 +0600

    chore: add Tailwind CSS integration and ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import tailwind from "eslint-plugin-tailwindcss";
 
 export default [
     ...eslintPluginAstro.configs.recommended,
+    ...tailwind.configs["flat/recommended"],
     {
         files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx,astro}"],
         ...jsxA11y.flatConfigs.strict,
