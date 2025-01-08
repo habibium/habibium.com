8e9d2d269cc359ebb388e94bb76d614aa58de96d
habibium <habib.prodev@gmail.com>
2025-01-08 14:18:26 +0600

    chore: update tailwind config with CSS vars

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                text: "var(--text)",
+                bg: "var(--bg)",
+                primary: "var(--primary)",
+                hyperlink: "var(--hyperlink)",
+                muted: "var(--muted)",
+                card: "var(--card)",
+            },
+        },
+    },
+    plugins: [],
+};
